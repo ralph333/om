28f3c2e84662c8d96780917917994ae8713ef419
ralph <lzs0728@126.com>
2017-05-02 17:38:50 +0800

    1

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,7 +7,21 @@ class LoginController extends Yaf_Controller_Abstract {
 	{
 		
 	}
+	public function authAction()
+	{
+	    $db_conn = new OmMysql ();
+	    $row = $db_conn->mysql_query("login_sql.login_check", array('username'=> $_POST['username'], 'password'=>md5($_POST['password'])));
 	
+	    header("content-type:application/json");
+	    $result = mysql_num_rows($row);
+	    if($result=="1")
+	    {
+	        session_start();
+	        $_SESSION['username']=$_POST['username'];
+	    }
+	    echo (json_encode($result));
+	    return false;
+	}
 	
 // 	public function logoutAction()
 // 	{
