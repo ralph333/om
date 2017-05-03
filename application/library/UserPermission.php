31d62bc9355f6102dc156f3f9cc9834be67f5faa
ralph <lzs0728@126.com>
2017-05-03 10:39:16 +0800

    3

--- application/library/UserPermission.php
+++ application/library/UserPermission.php
@@ -41,6 +41,7 @@ class UserPermission extends Yaf_Plugin_Abstract
 				$db_conn = new OmMysql ();
 				$row = $db_conn->mysqli_query("login_sql.password_check", array('username'=> $_SESSION['username']));
 				$result = mysql_fetch_array($row);
+				echo $result;
 				if($result['confirm_password'] == 0)
 				{
 					header("Location: /user/password");
