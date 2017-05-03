3f92e88b3a6a2367b927c56f6cd19e1290473570
ralph <lzs0728@126.com>
2017-05-03 11:30:01 +0800

    11

--- application/library/UserPermission.php
+++ application/library/UserPermission.php
@@ -43,9 +43,8 @@ class UserPermission extends Yaf_Plugin_Abstract
 				$row = $db_conn->mysql_query("login_sql.password_check", array('username'=> $_SESSION['username']));
 				
 				echo 1;
-				echo $row;
 				echo 2;
-				$result = mysql_fetch_array($row);
+				$result = mysqli_fetch_array($row);
 				print_r($result);
 				echo 3;
 				exit;
