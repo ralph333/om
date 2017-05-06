b6fdeba6ba3b4e78169779e15c9e6e01b741511a
ralph <lzs0728@126.com>
2017-05-06 11:25:33 +0800

    111

--- www/ztpubtop.js
+++ www/ztpubtop.js
@@ -1,21 +1,5 @@
 document.writeln("<style>");
-document.writeln("/*zt toppub start*/");
-document.writeln(".w1191Pub{width:1190px;margin:0 auto;}");
-document.writeln(".top{height:50px;background-color:#fff;padding-top:15px;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box;}");
-document.writeln(".top h1,.top ul li em,.top a.cBack,.footer .foot_wxh,.footer ul li em,.foot_right span{background:url(/zhuanti/js/images/njzttoppub.png) no-repeat;}");
-document.writeln(".top h1{width:251px;height:38px;background-position:-1px -2px;;display:block;float:left;}");
-document.writeln(".top h1 a{width:100%;height:100%;display:block;}");
-document.writeln(".top ul{height:33px;float:left;margin-left:120px;}");
-document.writeln(".top ul li{float:left;font-size:14px;line-height:31px;color:#000;}");
-document.writeln(".top ul li.top_li01{margin-right:100px;}");
-document.writeln(".top ul li a,.top ul li a:hover{width:100%;height:100%;color:#000;}");
-document.writeln(".top ul li em{display:block;float:left;margin-right:8px;}");
-document.writeln(".top ul li.top_li01 em{width:37px;height:31px;background-position:-497px -2px;}");
-document.writeln(".top ul li.top_li02 em{width:33px;height:34px;background-position:-550px -2px;}");
-document.writeln(".top ul li.top_li03 em{width:34px;height:34px;background-position:-594px -4px;}");
-document.writeln(".top a.cBack{width:78px;height:24px;display:block;background-position:-1px -64px;float:right;margin-top:5px;}");
-document.writeln(".top a.cBack:hover{background-position:-1px -118px;}");
-document.writeln("/*zt toppub end*/");
+
 document.writeln("</style>");
 document.writeln("<div class=\'top\'>");
 document.writeln("	<div class=\'w1191Pub\'>");
