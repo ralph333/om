8075391af27d7b8c8d7d53f4190d83177cd85602
ralph <lzs0728@126.com>
2017-05-06 11:28:00 +0800

    1111

--- www/ztpubtop.js
+++ www/ztpubtop.js
@@ -21,7 +21,7 @@ document.writeln("<div class=\'top\'>");
 document.writeln("	<div class=\'w1191Pub\'>");
 document.writeln("    	<h1><a href=\'/\' target=\'_self\'></a></h1>");
 document.writeln("        <ul>");
-document.writeln(" 			<li class=\'top_li01\'><a href=\"/zt/zlzt/\" target=\"\"><em></em>三七美科技美肤</a></li>");
+document.writeln(" 			<li class=\'top_li01\'><a href=\"#" target=\"\"><em></em>三七美科技美肤</a></li>");
 document.writeln("        </ul>");
 document.writeln("    </div>");
 document.writeln("</div>");
