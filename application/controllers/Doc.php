8a1f13e6789c7bf3227f0c1f8a16c4f5c2ed6f98
ralph <lzs0728@126.com>
2017-05-11 17:55:19 +0800

    1

--- application/controllers/Doc.php
+++ application/controllers/Doc.php
@@ -15,41 +15,50 @@ class DocController extends Yaf_Controller_Abstract {
 	
 	public function recordAction()
 	{
+	    $db_conn = new OmMysql();
 	    if (empty($_GET['action']))
 	    {
-	        echo "new";
+	        if (empty($_POST['doc_decs']) || empty($_POST['doc_user']) || empty($_POST['doc_password']) || empty($_POST['doc_group']) || empty($_POST['doc_common'])) {
+                echo '<script type="text/javascript">window.onload=function(){alert("所有选项必填。");window.top.location.href="/doc/om";}</script>';
+                exit();
+            }
+
+            $row = $db_conn->mysql_query("doc_sql.doc_record", array(
+                "description" => $_POST['doc_decs'],
+                "username" => $_POST['doc_user'],
+                "pass" => $_POST['doc_password'],
+                "belongs" => $_POST['doc_group'],
+                "common" => $_POST['doc_common']
+            ));
+            if (! $row) {
+                die('Error: ' . mysqli_error($db_conn));
+            }
+            echo '<script type="text/javascript">window.onload=function(){alert("添加成功");window.top.location.href="/doc/om";}</script>';
+            exit();
 	    }
 	    
 	    if ($_GET['action']== "update")
 	    {
-	        echo "update";
-	    }
-	    exit;
-	    if (empty($_POST['doc_decs']) ||
-	        empty($_POST['doc_user']) ||
-	        empty($_POST['doc_password']) ||
-	        empty($_POST['doc_group']) ||
-	        empty($_POST['doc_common']))
-	    {
-	        echo '<script type="text/javascript">window.onload=function(){alert("所有选项必填。");window.top.location.href="/doc/om";}</script>';
+	        $modify[] = array();
+	        if(!empty($_POST['doc_monify_user']))
+	        {
+	            $modify['user'] = $_POST['doc_monify_user'];
+	        }
+	        if(!empty($_POST['doc_monify_password']))
+	        {
+	            $modify['pass'] = $_POST['doc_monify_password'];
+	        }
+	        if(!empty($_POST['doc_monify_common']))
+	        {
+	            $modify['common'] = $_POST['doc_monify_common'];
+	        }
+	        print_r($modify);
 	        exit;
-	         
+
+	            
+	        
 	    }
 	    
-	    
-	    $db_conn = new OmMysql ();
-	    $row = $db_conn->mysql_query ( "doc_sql.doc_record", array("description"=>$_POST['doc_decs'], 
-	                                                               "username"=>$_POST['doc_user'], 
-	                                                               "pass"=>$_POST['doc_password'], 
-	                                                               "belongs"=>$_POST['doc_group'],
-	                                                               "common"=>$_POST['doc_common']
-	                                                              ));
-	    if(!$row)
-	    {
-	        die('Error: ' . mysqli_error($db_conn));
-	    }
-	    echo '<script type="text/javascript">window.onload=function(){alert("添加成功");window.top.location.href="/doc/om";}</script>';
-	    exit;
 	
 	}
 	
