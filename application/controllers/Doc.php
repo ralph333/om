3aa3c46fed96d262eaa8f3c79bbe5f1c98117da8
ralph <lzs0728@126.com>
2017-05-11 15:45:49 +0800

    11

--- application/controllers/Doc.php
+++ application/controllers/Doc.php
@@ -12,5 +12,14 @@ class DocController extends Yaf_Controller_Abstract {
 	{
 	
 	}
+	
+	public function recordAction()
+	{
+	    echo $_POST['doc_decs'];
+	    echo $_POST['doc_user'];
+	    echo $_POST['doc_password'];
+	    echo $_POST['doc_group'];
+	
+	}
 }
 ?>
