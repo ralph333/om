1cc5fbbb23813a3f8af0a5165cbcfc73c58f38a1
ralph <lzs0728@126.com>
2017-05-11 15:04:14 +0800

    111

---
+++ application/controllers/Doc.php
@@ -0,0 +1,12 @@
+<?php
+class DocController extends Yaf_Controller_Abstract {
+	public function init() {
+		$this->getView ()->setLayout ( 'PageHead' );
+	}
+	public function indexAction()
+	{
+		
+	}
+	
+}
+?>
