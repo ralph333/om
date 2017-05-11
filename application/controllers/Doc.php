883accfc7897705dad2af8e63f9c6bb4b3b395d7
ralph <lzs0728@126.com>
2017-05-11 16:42:01 +0800

    1

--- application/controllers/Doc.php
+++ application/controllers/Doc.php
@@ -18,7 +18,8 @@ class DocController extends Yaf_Controller_Abstract {
 	    if (empty($_POST['doc_decs']) ||
 	        empty($_POST['doc_user']) ||
 	        empty($_POST['doc_password']) ||
-	        empty($_POST['doc_group']))
+	        empty($_POST['doc_group']) ||
+	        empty($_POST['doc_common']))
 	    {
 	        echo '<script type="text/javascript">window.onload=function(){alert("所有选项必填。");window.top.location.href="/doc/om";}</script>';
 	        exit;
@@ -30,7 +31,8 @@ class DocController extends Yaf_Controller_Abstract {
 	    $row = $db_conn->mysql_query ( "doc_sql.doc_record", array("description"=>$_POST['doc_decs'], 
 	                                                               "username"=>$_POST['doc_user'], 
 	                                                               "pass"=>$_POST['doc_password'], 
-	                                                               "belongs"=>$_POST['doc_group']
+	                                                               "belongs"=>$_POST['doc_group'],
+	                                                               "common"=>$_POST['doc_common']
 	                                                              ));
 	    if(!$row)
 	    {
